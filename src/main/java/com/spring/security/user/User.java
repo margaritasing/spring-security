f8807de5c43b4e1e45121dd12c95c5895c22f5ab
margaritasing <libiamfreitesm@gmail.com>
2023-04-13 20:00:38 -0300

    primer commit

---
+++ src/main/java/com/spring/security/user/User.java
@@ -0,0 +1,23 @@
+package com.spring.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
